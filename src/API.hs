b019a259cc7947bbb0e39c7450cde90b1741341f
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-06-26 15:04:55 +0800

    Improve code in main

--- src/API.hs
+++ src/API.hs
@@ -5,12 +5,12 @@
 module API where
 
 import           Control.Concurrent
-import           Data.Bifunctor (first)
 import           Control.Exception          (bracket)
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader       (ask)
-import           Control.Monad.Trans.Except (ExceptT(..), runExceptT)
+import           Control.Monad.Trans.Except (ExceptT (..), runExceptT)
 import           Control.Monad.Trans.Reader (runReaderT)
+import           Data.Bifunctor             (first)
 import           Data.ByteString            (ByteString)
 import           Data.Pool
 import           Data.Text                  (unpack)
@@ -22,8 +22,8 @@ import           Servant
 import           Servant.Client
 
 import           API.Types
-import qualified Conf                       as Conf
-import qualified Conf.Types                 as Conf
+import           Conf
+import           Conf.Types
 import qualified DB.IssueTrackerDb          as DB
 import           Types
 import           Types.Error
@@ -65,61 +65,54 @@ app env = serve api $ enter (nt env) server
 runApp :: AppEnv -> IO ()
 runApp env = run 8080 (app env)
 
-initConnectionPool :: ConnectInfo -> IO (Pool Connection)
-initConnectionPool info =
-  createPool (connect info)
-             close
-             1
-             60
-             10
-
 postIssueC :: IssueBlueprint -> ClientM NoContent
 getIssuesC :: ClientM [Issue]
 postIssueC :<|> getIssuesC = client api
 
 data StartupError
-  = ConfError Conf.ConfigError
+  = ConfError ConfigError
   | DbInitErr
   deriving Show
 
--- prepareAppEnv :: IO (Either StartupError AppEnv)
--- prepareAppEnv =
---   where
+prepareAppEnv :: ExceptT StartupError IO AppEnv
+prepareAppEnv = do
+  conf <- initConf
+  pool <- initPool conf
 
-initConf :: ExceptT StartupError IO Conf.AppConf
-initConf = ExceptT . fmap (first ConfError) $ Conf.parseOptions "config.json"
+  pure (AppEnv conf (AppDb pool))
 
--- initPool :: Conf.AppConf -> ExceptT StartupError IO (Pool Connection)
--- initPool = ExceptT . _f $ createPool ()
+  where
+    initConf :: ExceptT StartupError IO AppConf
+    initConf = ExceptT . fmap (first ConfError) $ parseOptions "config.json"
+
+    initPool :: AppConf -> ExceptT StartupError IO (Pool Connection)
+    initPool conf =
+      let
+        dbConf = databaseConf conf
+        connectionInfo = defaultConnectInfo
+          { connectUser = unpack . getDbUser . databaseUser $ dbConf
+          , connectPassword = unpack . getDbPassword . databasePassword $ dbConf
+          , connectDatabase = unpack . getDbPath . databasePath $ dbConf
+          , connectPort = Conf.Types.getPort . databasePort $ dbConf
+          }
+      in
+        liftIO $ createPool (connect connectionInfo) close 1 60 10
+
+destroyEnv :: AppEnv -> IO ()
+destroyEnv = destroyAllResources . dbConn . appDb
 
 main :: IO ()
 main = do
-  conf <- runExceptT initConf
-  print conf
+  eEnv <- runExceptT prepareAppEnv
 
--- main :: IO ()
--- main = do
---   eAppConf <- parseOptions "config.json"
+  case eEnv of
+    (Left err) -> print err
+    (Right env) -> do
+      mgr <- newManager defaultManagerSettings
+      bracket (forkIO $ runApp env) killThread $ \_ -> do
+        ms <- flip runClientM (ClientEnv mgr (BaseUrl Http "localhost" 8080 "")) $ do
+          traverse postIssueC (IssueBlueprint "Testing blueprint" <$> (mkUserId 1))
+          getIssuesC
+        print ms
 
---   pure $ do
---     conf <- eAppConf
---     let connectionInfo
---           = defaultConnectInfo
---             { connectUser = unpack . getDbUser . dbUser $ conf
---             , connectPassword = unpack . getDbPassword . dbPassword $ conf
---             , connectDatabase = unpack . getDbPath . dbPath $ conf
---             , connectPort = Conf.Types.getPort . port $ conf
---             }
-
---   pool <- initConnectionPool connectionInfo
-
---   let
---     appDb = AppDb pool
---     env = AppEnv conf appDb
-
---   mgr <- newManager defaultManagerSettings
---   bracket (forkIO $ runApp env) killThread $ \_ -> do
---     ms <- flip runClientM (ClientEnv mgr (BaseUrl Http "localhost" 8080 "")) $ do
---       traverse postIssueC (IssueBlueprint "Testing blueprint" <$> (mkUserId 1))
---       getIssuesC
---     print ms
+      destroyEnv env
