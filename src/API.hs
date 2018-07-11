74458e44d4138fd6fbbf6770346cc884d611b8d8
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-07-11 13:24:43 +1000

    Get BasicAuth working

--- src/API.hs
+++ src/API.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators     #-}
 
@@ -10,15 +11,19 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Reader       (ask)
 import           Control.Monad.Trans.Except (ExceptT (..), runExceptT)
 import           Control.Monad.Trans.Reader (runReaderT)
+import           Data.Aeson
 import           Data.Bifunctor             (first)
 import           Data.ByteString            (ByteString)
 import           Data.Pool
 import           Data.Text                  (unpack)
 import qualified Data.Time.Clock            as T
 import           Database.PostgreSQL.Simple
+import           GHC.Generics
 import           Network.HTTP.Client        (defaultManagerSettings, newManager)
 import           Network.Wai.Handler.Warp
-import           Servant
+import           Servant                    as S
+import           Servant.Auth               as SA
+import           Servant.Auth.Server        as SA
 import           Servant.Client
 
 import           API.Types
@@ -33,13 +38,38 @@ import           Types.User
 import           Layer1
 import           Layer2
 
+data AuthenticatedUser = AUser { auId :: UserId
+                               } deriving (Show, Generic)
+
+instance ToJSON AuthenticatedUser
+instance FromJSON AuthenticatedUser
+instance ToJWT AuthenticatedUser
+instance FromJWT AuthenticatedUser
+
 type API = ReqBody '[JSON] IssueBlueprint :> Post '[JSON] NoContent
       :<|> Get '[JSON] [Issue]
+
+type APIServer =
+  Auth '[SA.BasicAuth] AuthenticatedUser :> API
+
+type APIClient =
+  S.BasicAuth "test" AuthenticatedUser :> API
 
 api :: Proxy API
 api = Proxy
 
 type AppM = AppT Handler
+
+basicAuthServerContext :: Context (BasicAuthCheck AuthenticatedUser ': '[])
+basicAuthServerContext = authCheck S.:. EmptyContext
+
+authCheck :: BasicAuthCheck AuthenticatedUser
+authCheck =
+  let check (BasicAuthData username password) =
+        if username == "servant" && password == "server"
+        then either (const $ pure Unauthorized) pure $ (fmap (Authorized . AUser) (mkUserId 1))
+        else pure Unauthorized
+  in BasicAuthCheck check
 
 server :: ServerT API AppM
 server = postIssueHandler :<|> getIssuesHandler
@@ -60,14 +90,14 @@ nt env = NT $ ((either (throwError . toServantErr) pure) =<<)
     toServantErr None = err404 { errBody = "Unknown Error"}
 
 app :: AppEnv -> Application
-app env = serve api $ enter (nt env) server
+app env = serveWithContext api basicAuthServerContext $ enter (nt env) server
 
 runApp :: AppEnv -> IO ()
 runApp env = run 8080 (app env)
 
 postIssueC :: IssueBlueprint -> ClientM NoContent
 getIssuesC :: ClientM [Issue]
-postIssueC :<|> getIssuesC = client api
+postIssueC :<|> getIssuesC = client (Proxy :: Proxy APIClient) (BasicAuthData "servant" "server")
 
 data StartupError
   = ConfError ConfigError
